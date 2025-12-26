80475350876cb05ec12d06041ce37e93364977b1
bund <bunsdev08@gmail.com>
2025-12-26 15:25:01 +0900

    update gitignote

--- config/Database.php
+++ config/Database.php
@@ -6,9 +6,9 @@ class Database {
     public static function getConfig(): array {
         return [
             'host' => $_ENV['DB_HOST'] ?? 'localhost',
-            'user' => $_ENV['DB_USER'] ?? 'admin',
-            'pass' => $_ENV['DB_PASS'] ?? '#apacoba',
-            'name' => $_ENV['DB_NAME'] ?? 'lms_school_db',
+            'user' => $_ENV['DB_USER'] ?? 'user',
+            'pass' => $_ENV['DB_PASS'] ?? '',
+            'name' => $_ENV['DB_NAME'] ?? 'db_name',
         ];
     }
 }
